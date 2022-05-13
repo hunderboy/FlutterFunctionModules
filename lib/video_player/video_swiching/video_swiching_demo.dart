0d9e9aa06196de58148dad5a9dcb56f6616d7da9
hunderboy <masury2011@gmail.com>
2022-05-13 16:09:00 +0900

    비디오 플레이어 분석 완료

--- lib/video_player/video_swiching/video_swiching_demo.dart
+++ lib/video_player/video_swiching/video_swiching_demo.dart
@@ -12,9 +12,7 @@ class _VideoSwichingDemoState extends State<VideoSwichingDemo> {
   int index = 0;
   double _position = 0;
   double _buffer = 0;
-  bool _lock = true;
-  Map<String, VideoPlayerController> _controllers = {};
-  Map<int, VoidCallback> _listeners = {};
+  bool _lock = true; // 영상 재생 잠금
   final Set<String> _urls = {
     'https://resources.mora.kr/static/exercise/media/507/M20/267a1d83d7d2fbf451be5573a17cd38d.mp4',
     'https://resources.mora.kr/static/exercise/media/749/M5/ba0b4e3b8503e4d7628ae599e434ac27.mp4',
@@ -28,30 +26,28 @@ class _VideoSwichingDemoState extends State<VideoSwichingDemo> {
     'https://resources.mora.kr/static/exercise/media/907/M20/26901613dcee04449688631ea6a8d6d3.mp4',
   };
 
-  @override
-  void initState() {
-    super.initState();
-    /// _urls 길이가 0 보다 크면
-    if (_urls.length > 0) {
-      // 컨트롤러를 초기화 한다.
-      _initController(0).then((_) {
-        // 초기화가 끝남녀
-        _playController(0);
-      });
-    }
-    /// _urls 길이가 1 보다 크면
-    if (_urls.length > 1) {
-      _initController(1).whenComplete(
-              () => _lock = false
-      );
-    }
-  }
+  /// key : _urls.elementAt(index) 예를 들면 'https://resources.mora.kr/static/exercise/media/507/M20/267a1d83d7d2fbf451be5573a17cd38d.mp4' 이게 Key 가 됨.
+  /// value : VideoPlayerController
+  /// [todo] 예시 ------------------------------------------------
+  /// { 'https://resources.mora.kr/static/exercise/media/507/M20/267a1d83d7d2fbf451be5573a17cd38d.mp4' , VideoPlayerController}
+  /// { 'https://resources.mora.kr/static/exercise/media/749/M5/ba0b4e3b8503e4d7628ae599e434ac27.mp4' , VideoPlayerController}
+  Map<String, VideoPlayerController> _controllers = {};
+  /// key : index 값
+  /// value : _listenerSpawner(index) = VoidCallback
+  /// [todo] 예시 ------------------------------------------------
+  /// { 0 , _listenerSpawner(0)}
+  /// { 1 , _listenerSpawner(1)}
+  Map<int, VoidCallback> _listeners = {};
+
+
+
 
   VoidCallback _listenerSpawner(index) {
     return () {
       int dur = _controller(index)!.value.duration.inMilliseconds;
       int pos = _controller(index)!.value.position.inMilliseconds;
-      int buf = _controller(index)!.value.buffered.last.end.inMilliseconds;
+      // int buf = _controller(index)!.value.buffered.last.end.inMilliseconds;
+      int buf = 0;
 
       setState(() {
         if (dur <= pos) {
@@ -64,12 +60,39 @@ class _VideoSwichingDemoState extends State<VideoSwichingDemo> {
       // print("dur : $dur");
       // print("---------------- pos : $pos");
 
+      /// 끝나면
       if (dur - pos < 1) {
         if (index < _urls.length - 1) {
-          _nextVideo();
+          _nextVideo(); // 다음 비디오 재생
         }
       }
     };
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// [todo] 결론 : VideoContorller 초기화는 현재 재생 [index,index+1] 이렇게 2개만 설정한다.
+    if (_urls.isNotEmpty) {
+      // index=0 번째 컨트롤러를 초기화 한다. (시작하는 videoController 초기화)
+      _initController(0).then((_) {
+        _playController(0); // index=0 번째 초기화가 끝나면 영상을 재생 한다.
+      });
+
+      /// 1번째 인덱스까지 Controller 가 초기화가 완료 되면
+      /// _lock = false 설정한다. (영상 재생 잠금 해제의 의미)
+      _initController(1).whenComplete(
+        () => _lock = false
+      );
+    }
+
+
+    /// [todo] then vs whenComplete
+    /// Then과 whenComplete는 역할이 다르다.
+    /// 1. Then은 Error가 없다면 결괏값을 value로 받아서 실행한다. 하지만, Error가 Return 되면 출력이 되지 않는다.
+    /// 2. WhenComplete는 Future가 끝나면 무조건 실행된다.(Error 발생 유무에 관계가 없다.)
+    /// catchError는 Error가 Return이 될 때 작동하며, Error가 Return 되더라도 catchError가 있다면 Then이 실행된다.
   }
 
   VideoPlayerController? _controller(int index) {
@@ -79,46 +102,53 @@ class _VideoSwichingDemoState extends State<VideoSwichingDemo> {
   Future<void> _initController(int index) async {
     var controller = VideoPlayerController.network(_urls.elementAt(index));
     // var controller = VideoPlayerController.asset('assets/video/'+_urls.elementAt(index)+'.mp4');
-    _controllers[_urls.elementAt(index)] = controller;
-    await controller.initialize();
-    print("사이즈 : "+_controllers.length.toString());
-  }
-
-  void _removeController(int index) {
-    _controller(index)!.dispose();
-    _controllers.remove(_urls.elementAt(index));
-    _listeners.remove(index);
-  }
-
-  void _stopController(int index) {
-    _controller(index)!.removeListener(_listeners[index]!);
-    _controller(index)!.pause();
-    _controller(index)!.seekTo(Duration(milliseconds: 0));
+    // await controller.initialize();
+    await controller.initialize().then((_) {
+      _controllers[_urls.elementAt(index)] = controller; /// 초기화 완료 되면 _controllers 에 추가한다.
+      print("키 이름 : "+_urls.elementAt(index));
+      print("_controllers 사이즈 : "+_controllers.length.toString());
+    });
   }
 
   void _playController(int index) async {
-    if (!_listeners.keys.contains(index)) { /// 리스너들에 해당 인덱스의 키를 포함하지 않고 있다면,
+    if (!_listeners.keys.contains(index)) { /// _listeners 의 key 들 중에 해당 index 값이 없는 경우
       _listeners[index] = _listenerSpawner(index); /// 추가한다.
     }
+    print("_listeners 사이즈 : "+_listeners.length.toString());
     _controller(index)!.addListener(_listeners[index]!);
     await _controller(index)!.play();
     setState(() {});
+  }
+
+  void _removeController(int index) {
+    _controller(index)!.dispose(); // 컨트롤러 해제
+    _controllers.remove(_urls.elementAt(index)); // 컨트롤러 맵에서 컨트롤러 제거
+    _listeners.remove(index); // 리스너도 제거
+  }
+
+  void _stopController(int index) {
+    _controller(index)!.removeListener(_listeners[index]!); // 컨트롤러에서 리스너 제거
+    _controller(index)!.pause();
+    _controller(index)!.seekTo(Duration(milliseconds: 0));
   }
 
   void _previousVideo() {
     if (_lock || index == 0) {
       return;
     }
-    _lock = true;
 
+    _lock = true;
     _stopController(index);
 
+    /// 다음 인덱스의 비디오 컨트롤러 제거
     if (index + 1 < _urls.length) {
       _removeController(index + 1);
     }
 
+    /// 이전 영상 재생 (이전 영상의 컨트롤러는 0초 상태로 세팅 된 후, 다음재생 되게끔 되어 있음.)
     _playController(--index);
 
+    /// 이전 영상 재생을 위한 컨트롤러 초기화 세팅
     if (index == 0) {
       _lock = false;
     } else {
@@ -127,30 +157,29 @@ class _VideoSwichingDemoState extends State<VideoSwichingDemo> {
   }
 
   void _nextVideo() async {
+    /// 만약, _lock = true or index 가 마지막 인덱스 인경우
     if (_lock || index == _urls.length - 1) {
       return;
     }
-    _lock = true;
 
+    _lock = true;
     _stopController(index);
 
+    /// 이전 인덱스의 비디오 컨트롤러 제거
     if (index - 1 >= 0) {
       _removeController(index - 1);
     }
 
+    /// 다음 영상 재생 (다음영상의 컨트롤러는 이전 영상 재생될때 이미 초기화 되었음)
     _playController(++index);
 
+    /// 다음 영상 재생을 위한 컨트롤러 초기화 세팅
     if (index == _urls.length - 1) {
       _lock = false;
     } else {
       _initController(index + 1).whenComplete(() => _lock = false);
     }
   }
-
-
-
-
-
 
 
   @override
@@ -160,7 +189,7 @@ class _VideoSwichingDemoState extends State<VideoSwichingDemo> {
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarBrightness: Brightness.light,   // ios 상태바 텍스트 컬러 = black
         ),
-        title: Text("Playing index $index of total index ${_urls.length-1}"),
+        title: Text("index $index of ${_urls.length-1}"),
       ),
       body: Stack(
         children: <Widget>[
@@ -197,9 +226,13 @@ class _VideoSwichingDemoState extends State<VideoSwichingDemo> {
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          FloatingActionButton(onPressed: _previousVideo, child: Icon(FontAwesomeIcons.arrowLeft)),
+          FloatingActionButton(
+            heroTag: 'previous',
+              onPressed: _previousVideo, child: Icon(FontAwesomeIcons.arrowLeft)),
           SizedBox(width: 24),
-          FloatingActionButton(onPressed: _nextVideo, child: Icon(FontAwesomeIcons.arrowRight)),
+          FloatingActionButton(
+              heroTag: 'next',
+              onPressed: _nextVideo, child: Icon(FontAwesomeIcons.arrowRight)),
         ],
       ),
     );
