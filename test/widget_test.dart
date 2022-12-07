5d9dfbfe5ea7bbbb8a4dde8d1b0049fcbb1cfb6a
hunderboy <masury2011@gmail.com>
2022-12-07 17:05:35 +0900

    Feat: 위젯 테스트 추가
    테스트를 위한 MyWidget 코드를 작성하고, 위젯테스트에 적용중
    Not found: 'dart:ui' 에러 발생하여 해결중
    
    Fixes: #123
    Ref: #56
    Related to: #48, #45

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,26 +6,34 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_function_modules/widget_test/testing_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_function_modules/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    // await tester.pumpWidget(const MyApp());
-    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MyWidget(title: 'T', message: 'M'));
   });
+
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   // await tester.pumpWidget(const MyApp());
+  //   await tester.pumpWidget(MyApp());
+  //
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
+  //
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
+  //
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+
 }
